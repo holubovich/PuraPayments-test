39c711dace75d5c6c4e9da37ac874e0cadc66afe
Elena Golubovich <elenaholubovich988@gmail.com>
2019-02-12 23:41:08 +0300

    add body

--- sections/body.php
+++ sections/body.php
@@ -1,3 +1,37 @@
 <div class="main">
+    <div class="solutions">
+        Tailored Merchant Processing Solutions for Your Business
+    </div>
+    <div class="panel">
+        <div class="left__panel">
+            <div class="hdr__panel">pura rayments offers:</div>
+            <ul>
+                <li><span class="colortext">Competitive</span> Rates starting at 0.40%</li>
+                <li><span class="colortext">24/7</span> Dedicated customer<br/> support team</li>
+                <li><span class="colortext">EMV and PCI compliant</span> <br/> plus NFC capable equipment</li>
+                <li><span class="colortext">Fast & Easy</span> applicathion process with <strong> no applicathion fees!</strong></li>
+            </ul>
+            <div class="phone">
+                <div class="button">
+                    <div class="button-phone"></div>
+                </div>
+                <div class="phone_text">
+                    <span class="phone-text">call for a free quote</span>
+                    <div class="phone-number">888-888-8888</div>
+                </div>
+            </div>
+
+        </div>
+        <div class="right__panel">
+                <div class="hdr__panel__right">*<span class="colortext">free</span> terminal</div>
+                <ul>
+                    <li><span class="colortext-right">Retail/Swipe</span></li>
+                    <li><span class="colortext-right">eCommerce</span></li>
+                    <li><span class="colortext-right">Wireless</span></li>
+                    <li><span class="colortext-right">Mobile</span></li>
+                    <li><span class="colortext-right">Mail Order/Phone</span></li>
+                </ul>
+        </div>
+    </div>
 
 </div>
